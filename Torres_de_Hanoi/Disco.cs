898b8b29e45fe6eee8e1b07bef73a3d46268bf35
KB <krystian8844@gmail.com>
2022-03-21 13:39:38 +0100

    Implementada clase Disco.

---
+++ Torres_de_Hanoi/Disco.cs
@@ -0,0 +1,17 @@
+﻿namespace Torres_de_Hanoi
+{
+    class Disco
+    {
+        //Objecto disco, para insertar en la torre.
+
+
+        public int Valor { get; set; } // El tamaño (valor) del disco.
+
+        //Constructor
+        public Disco(int val)
+        {
+            this.Valor = val;
+        }
+    }
+}
+        
